96fc97ed741ac6b5f001a3d8bf394bae7d19e6c3
mi-1109 <meisuzuki.sophia@gmail.com>
2021-04-15 06:40:51 +0000

    close #9

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,20 @@
 Rails.application.routes.draw do
   devise_for :admins
   devise_for :customers
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resource :customers do
+     get 'quit_confirm', on: :collection
+  end
+
+  resources :adresess, only: [:index, :create, :edit, :update, :destroy]
+  resources :genres, only: [:index, :create, :edit, :update]
+  resources :orders, only: [:index, :show, :order_status, :making_status, :order_info, :order_confirm, :order_complete]
+  resources :products, only: [:index, :show, :new, :create, :edit, :update]
+  resources :cart_products, only: [:index, :create, :update, :destroy]
+
+  resource :homes do
+    collection do
+     get :about, :shoplist
+    end
+  end
 end
