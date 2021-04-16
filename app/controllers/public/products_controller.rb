b94579468cf552e2f3d105663ff655094dc83a1d
EC2 Default User <ec2-user@ip-172-31-90-200.ec2.internal>
2021-04-16 09:56:27 +0000

    [add] ヘッダーとフッター追加

--- app/controllers/public/products_controller.rb
+++ app/controllers/public/products_controller.rb
@@ -1,2 +1,4 @@
 class Public::ProductsController < ApplicationController
+  def index
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 2021_04_15_062123) do
     t.boolean "is_deleted"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
