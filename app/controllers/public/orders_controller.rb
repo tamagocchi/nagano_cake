2ae52124e759787f953678c6345e77169f2a12ac
Taka2401 <tkht2401@gmail.com>
2021-04-17 11:16:37 +0000

    [add] 注文関連機能

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,2 +1,28 @@
 class Public::OrdersController < ApplicationController
+
+  def index
+    @orders = Order.all
+  end
+
+  def show
+    @order = User.cart_products
+  end
+
+  def new
+  end
+
+  def confirm
+    @order = User.order
+  end
+
+  def create
+    @order = Order.new
+    @order.save
+    redirect_to orders_complete_path
+  end
+
+  def complete
+  end
+
+
 end
