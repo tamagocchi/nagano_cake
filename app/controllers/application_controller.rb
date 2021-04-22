75000c2f1dad861e53c12814a82bd934958b39d2
mi-1109 <meisuzuki.sophia@gmail.com>
2021-04-22 12:57:08 +0000

    [Fix] Footer Layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,16 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
 
-# ====== ログイン・ログアウト後の遷移先 ==========
+# ====== 新規登録・ログイン・ログアウト後の遷移先 ==========
   private
+
+  def after_sign_in_path_for(resource_or_scope)
+    if resource_or_scope.is_a?(Admin)
+      admin_top_path
+    else resource_or_scope == :customer
+      root_path
+    end
+  end
 
   def after_sign_out_path_for(resource_or_scope)
     if resource_or_scope == :customer
@@ -13,14 +21,6 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  def after_sign_in_path_for(resource_or_scope)
-    if resource_or_scope == :customer
-      root_path
-    else resource_or_scope == :admin
-      new_admin_session_path
-    end
-  end
-  
   protected
 
   def configure_permitted_parameters
--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -1,4 +1,7 @@
 class Public::RegistrationsController < ApplicationController
-    
-    
+
+  def after_sign_up_path_for(resource)
+    customer_show_path(current_customer)
+  end
+
 end
