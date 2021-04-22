11d151059430c51632b32c67a6c996417780100e
mi-1109 <meisuzuki.sophia@gmail.com>
2021-04-22 11:37:56 +0000

    [Add]ログアウト後の遷移処理

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,25 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
 
-  def after_sign_out_path_for(resource)
-    root_path
+# ====== ログイン・ログアウト後の遷移先 ==========
+  private
+
+  def after_sign_out_path_for(resource_or_scope)
+    if resource_or_scope == :customer
+      new_customer_session_path
+    else resource_or_scope == :admin
+      new_admin_session_path
+    end
   end
 
+  def after_sign_in_path_for(resource_or_scope)
+    if resource_or_scope == :customer
+      root_path
+    else resource_or_scope == :admin
+      new_admin_session_path
+    end
+  end
+  
   protected
 
   def configure_permitted_parameters
