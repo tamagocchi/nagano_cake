5b615533c844758cc4af4eec7ffadd23f908b289
mi-1109 <meisuzuki.sophia@gmail.com>
2021-04-24 14:27:45 +0000

    [Fix]住所新規登録のバグを修正

--- db/schema.rb
+++ db/schema.rb
@@ -54,10 +54,9 @@ ActiveRecord::Schema.define(version: 2021_04_22_133302) do
     t.string "phone_number"
     t.string "postcode"
     t.string "address"
-    t.boolean "is_deleted"
+    t.boolean "is_deleted", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "name"
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
