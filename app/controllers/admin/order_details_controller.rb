0d221521f51ff14c4701abd2d60c95a73a62e2d6
Taka2401 <tkht2401@gmail.com>
2021-04-24 02:19:55 +0000

    [add] admin/注文履歴一覧

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,2 +1,21 @@
 class Admin::OrderDetailsController < ApplicationController
+
+  def update
+    @order_detail = OrderDetail.find(params[:order_id])
+    @order_detail.update(order_detail_params)
+
+    @order = @order_detail.order
+
+   if @order.order_details.where(making_status: '製作中').count > 0
+      @order.order_status = '製作中'
+      @order.save
+   end
+
+    redirect_to request.referer
+  end
+
+  def order_detail_params
+    params.require(:order_detail).permit(:order_id, :making_status, :product_quantity, :order_id)
+  end
+
 end
