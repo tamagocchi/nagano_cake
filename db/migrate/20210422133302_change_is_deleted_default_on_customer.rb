b3b1adebb9f322d8fb6461ef942eb85ecfa73109
Taka2401 <tkht2401@gmail.com>
2021-04-25 04:17:45 +0000

    test

--- db/migrate/20210422133302_change_is_deleted_default_on_customer.rb
+++
@@ -1,5 +0,0 @@
-class ChangeIsDeletedDefaultOnCustomer < ActiveRecord::Migration[5.2]
-  def up
-    change_column :customers, :is_deleted, :boolean, default: false, null: false
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_22_133302) do
+ActiveRecord::Schema.define(version: 2021_04_22_063902) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "customer_id"
@@ -54,9 +54,10 @@ ActiveRecord::Schema.define(version: 2021_04_22_133302) do
     t.string "phone_number"
     t.string "postcode"
     t.string "address"
-    t.boolean "is_deleted", default: false, null: false
+    t.boolean "is_deleted"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
